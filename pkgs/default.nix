58c123cf58f9a5d3eaddd13dee0cd8cabd2de0f8
Matt White <mail@matt-w.net>
2024-11-03 14:00:38 -0700

    remove desk and cursor-bin

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,9 +1,7 @@
 { pkgs ? import <nixpkgs> {}, lib ? pkgs.lib }:
 
 {
-  cursor-bin = pkgs.callPackage ./cursor-bin {};
   blurlock = pkgs.callPackage ./blurlock {};
   i3exit = pkgs.callPackage ./i3exit {};
   cursor-server = pkgs.callPackage ./cursor-server {};
-  desk = pkgs.callPackage ./desk {};
 }

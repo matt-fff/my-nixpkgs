a35d4f3730b06bee9911b312eb0911d093db18b6
Matt White <mail@matt-w.net>
2024-11-17 18:24:08 -0700

    phinger hyprcursor

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,4 +5,6 @@
   i3exit = pkgs.callPackage ./i3exit {};
   cursor-server = pkgs.callPackage ./cursor-server {};
   hyprwall = pkgs.callPackage ./hyprwall {};
+  hyprland = pkgs.callPackage ./hyprland {};
+  phinger-hyprcursor = pkgs.callPackage ./phinger-hyprcursor {};
 }
